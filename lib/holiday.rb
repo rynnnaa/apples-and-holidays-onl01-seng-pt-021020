5248d53a1806b65e67e4d2e3b3efdfefca407ee7
Ryna Truong <rynatruong@outlook.com>
2020-02-24 05:31:26 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:summer][:fourth_of_july][1]
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -21,26 +22,32 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+  
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
+  holiday_hash[season][holiday_name] = supply_array
+    # code here
   # remember to return the updated hash
+  return holiday_hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+  
+  holiday_hash[:winter].map do |holiday, supplies|
+    supplies
+  end.flatten
   # return an array of all of the supplies that are used in the winter season
 
 end
@@ -53,12 +60,25 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+    holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |day, supplies|
+      puts "  #{day.to_s.split('_').map {|word| word.capitalize}.join(' ')}: #{supplies.join(", ")}"
+    end
+  end
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
+    end
+  end.flatten.compact
 
 end
 
